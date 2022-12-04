63d531fa483f6dd53676ddc9433faa6fb8c362e6
bilguun-enkh <bilguunenkh21@gmail.com>
2022-12-04 12:18:45 +0800

    12/04/2022 third commit

--- assignments/jsAssignments/guessTheNumber.js
+++ assignments/jsAssignments/guessTheNumber.js
@@ -6,10 +6,8 @@ let input = Number(prompt("Guess the number from 1 to 100."))
 
 while ( input != randNum) {
     if (input > randNum) {
-        console.log("Your number is higher than the random number.")
-    } else {
-        console.log("Your number is lower than the random number.")
+        input = Number(prompt("Your number is higher than the random number."))
     }
-    input = Number(prompt("Guess again"))
+    input = Number(prompt("Your number is lower than the random number."))
 } 
 console.log(`You got it correct! The number was ${randNum}.`)
