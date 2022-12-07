de9205952c4ab1e641ae96a3cdeb70c4f792e053
Morning <morning@admins-iMac-3.local>
2022-12-07 12:22:09 +0800

    12/07/2022 commit

---
+++ day-17/js/array.js
@@ -0,0 +1,112 @@
+console.log("Array Methods.");
+
+function findAt(arr, index) {
+  return arr[index];
+}
+
+const array1 = [1, 2, 3, 4, 5];
+console.log(findAt(array1, 0));
+console.log(findAt(array1, 4));
+
+console.log(array1.at(0));
+console.log(array1.at(4));
+
+
+
+
+function arrConcat(arr1, arr2) {
+  let concatedArray = [];
+  for (let i = 0; i < arr2.length; i++) {
+    concatedArray.push(arr1[i]);
+  }
+  for (let i = 0; i < arr1.length; i++) {
+    concatedArray.push(arr2[i]);
+  }
+  return concatedArray;
+}
+let arrayC1 = [1, 2, 3];
+let arrayC2 = [4, 5, 6];
+console.log(arrConcat(arrayC1, arrayC2));
+console.log(arrConcat(arrayC2, arrayC1));
+
+console.log(arrayC1.concat(arrayC2));
+console.log(arrayC1 + "," + arrayC2);
+
+
+
+
+function arrayElementAdd(arr1, arr2) {
+  let sum = [];
+  for (let i = 0; i < arr1.length; i++) {
+    sum[i] = arr1[i] + arr2[i];
+  }
+  return sum;
+}
+console.log(arrayElementAdd(arrayC1, arrayC2));
+
+
+
+
+let arrayIncludes = function (array, number) {
+  let found = false;
+  for (let i = 0; i < array.length; i++)
+    if (number === array[i]) {
+      found = true;
+      break;
+    }
+  return found;
+};
+
+const arrayInc = [2, 4, 5];
+console.log(arrayIncludes(arrayInc, 2));
+console.log(arrayIncludes(arrayInc, 5));
+console.log(arrayIncludes(arrayInc, 3));
+
+console.log(arrayInc.includes(2));
+console.log(arrayInc.includes(5));
+console.log(arrayInc.includes(3));
+
+let findIndex = function (array, number) {
+  let index = -1
+  for (let i = 0; i < array.length; i++) {
+    if (number === array[i]) {
+      index = i
+    }
+  }
+  return index
+}
+const arrayIndex = [1, 2, 3]
+console.log(findIndex(arrayIndex, 3))
+console.log(findIndex(arrayIndex, 2))
+console.log(findIndex(arrayIndex, 10))
+
+console.log(arrayIndex.indexOf(3));
+
+let funcJoin = (array, symbol) => {
+  let output = ''
+  for (let i = 0; i < array.length; i++) {
+    output = output + array[i] + symbol
+  }
+  return output
+}
+
+
+const arrayJoin = [1, 2, 3]
+console.log(funcJoin(arrayJoin, "+"));
+
+
+let popFunc = (array) => {
+  const lastIndex = array.length - 1
+  let poppedArray = []
+
+  for (let i = 0; i < array.length; i++) {
+    if ( i != lastIndex) {
+      poppedArray.push(array[i])
+    }
+  }
+
+  return poppedArray
+}
+
+const popArray = [1, 2, 3]
+console.log(popFunc([12,4,3,5,6]))
---
+++ day-17/js/main.js
@@ -0,0 +1,41 @@
+function findMin(x, y) {
+  if (x < y) {
+    return x;
+  } else {
+    return y;
+  }
+}
+let result = findMin(2, 3);
+console.log("exercise no1 " + result);
+
+function printArray(array) {
+  for (let i = 0; i < array.length; i++) {
+    console.log("exercise no2 " + array[i]);
+  }
+}
+
+printArray([5, 10, 15, 20, 25]);
+
+let findArrayMax = function (arr) {
+  let max = -1;
+  for (let i = 0; i < arr.length; i++) {
+    if (max <= arr[i]) {
+      max = arr[i];
+    }
+  }
+  return max;
+};
+
+let array = [1, 2, 3, 4, 5, 6, 2, 23, 15, 300];
+console.log("exercise no3 " + findArrayMax(array));
+
+let findArrayAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum = sum + arr[i];
+  }
+  const average = sum / arr.length;
+  return average;
+};
+let array2 = [2,4,6,8,10,12,14]
+console.log("exercise no4 " + findArrayAverage(array2));
