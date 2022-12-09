d297805039f4534a5680ba30eae019c3d461393e
Morning <morning@admins-iMac-3.local>
2022-12-09 10:21:42 +0800

    quick fix

--- day-19/assignment/animalsObject.js
+++ day-19/assignment/animalsObject.js
@@ -14,7 +14,7 @@ let animals = [{
 
 function whatDoesTheFoxSay(array, animalType) {
     for (let i = 0; i < array.length; i++) {
-        if ( animalType == animals[i].type)
+        if ( array[i].type == animalType)
         console.log(`${array[i].sound} - ${array[i].sound} - ${array[i].sound}`)
     }
 }
---
+++ day-19/assignment/students.js
@@ -0,0 +1,21 @@
+let students = [
+    {
+        name: "Bilguun",
+        birthOfYear: '1999',
+        hobby: 'Sleeping',
+        Single: 'True',
+    },
+    {
+        name: "Bold",
+        birthOfYear: '1952',
+        hobby: 'Dancing',
+        Single: 'False',
+    },
+    {
+        name: "Sugar",
+        birthOfYear: '1998',
+        hobby: 'Running',
+        Single: 'False',
+    }
+]
+
