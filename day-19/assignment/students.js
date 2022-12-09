305a5083ae85053d7c96571859194c8d1854abcb
Morning <morning@admins-iMac-3.local>
2022-12-09 10:35:01 +0800

    12/09/2022 commit

--- day-19/assignment/students.js
+++ day-19/assignment/students.js
@@ -1,21 +1,52 @@
 let students = [
     {
         name: "Bilguun",
-        birthOfYear: '1999',
+        birthOfYear: 1999,
         hobby: 'Sleeping',
-        Single: 'True',
+        single: true
     },
     {
         name: "Bold",
-        birthOfYear: '1952',
+        birthOfYear: 1952,
         hobby: 'Dancing',
-        Single: 'False',
+        single: false,
     },
     {
         name: "Sugar",
-        birthOfYear: '1998',
+        birthOfYear: 1998,
         hobby: 'Running',
-        Single: 'False',
+        single: false,
     }
 ]
 
+function findOld(array) {
+    for (let i = 0; i < array.length; i++) {
+        let tempBirth = 2022 - array[i].birthOfYear
+        if (tempBirth > 20) {
+            console.log(array[i].name)
+        }
+    }
+}
+findOld(students)
+
+function findAge(array, name) {
+    let age = 0
+    for (let i = 0; i < array.length; i++) {
+        if (name == array[i].name) {
+            let age = 2022 - array[i].birthOfYear
+            return age
+        }
+    }
+}
+
+console.log("Age of student " +findAge(students, "Bold"))
+
+function single(array) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i].single == true) {
+            console.log(`Single students: ${array[i].name}`)
+        }
+    }
+}
+
+single(students)
