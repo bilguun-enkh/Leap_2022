4d56ce9c5b81f6ca24f563bb504493e4db7ea0b2
bilguun-enkh <bilguunenkh21@gmail.com>
2022-12-09 15:11:19 +0800

    12/09/2022

---
+++ day-19/assignment/simpleObject.js
@@ -0,0 +1,16 @@
+let dogTamir = {}
+console.log(dogTamir)
+
+dogTamir = {
+    breed: "Husky",
+    name: "Tamir",
+    color: "Black",
+    age: "211",
+    trait: "Barks alot",
+}
+
+console.log(dogTamir.breed)
+console.log(dogTamir.name)
+console.log(dogTamir.color)
+console.log(dogTamir.age)
+console.log(dogTamir.trait)
